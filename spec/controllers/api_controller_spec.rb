e1618d436baccca67b749f5ce79cf6e7b4f06b4c
David Elner <david@davidelner.com>
2015-08-29 20:24:54 -0400

    Fixed: ApiController examples (client is now protected)

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -8,13 +8,6 @@ describe Butterfli::Instagram::Rails::ApiController do
       let(:client_id) { "client_id" }
       let(:client_secret) { "client_secret" }
       before { configure_for_instagram(client_id, client_secret) }
-      
-      describe "#client" do
-        subject { super().client }
-        it { expect(subject).to be_a_kind_of(Instagram::Client) }
-        it { expect(subject.client_id).to eq(client_id) }
-        it { expect(subject.client_secret).to eq(client_secret) }
-      end
     end
   end
 end
