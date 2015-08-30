0e4b103ba9a0a397005787a00fbb6425b63943e6
David Elner <david@davidelner.com>
2015-08-29 20:10:36 -0400

    Added: Signature verification for Instagram API calls

--- app/controllers/butterfli/instagram/rails/api_controller.rb
+++ app/controllers/butterfli/instagram/rails/api_controller.rb
@@ -1,5 +1,12 @@
 class Butterfli::Instagram::Rails::ApiController < Butterfli::Controller
+  before_action :is_signed?
+
   def client
     @client ||= Butterfli.configuration.providers(:instagram).client
+  end
+
+  private
+  def is_signed?
+    client.validate_update(request.body, headers)
   end
 end
